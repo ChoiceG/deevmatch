a2b5bcea762047faf9351fe98aa615b319a91853
ChoiceG <efechoice@gmail.com>
2024-12-12 17:48:54 +0100

    Updated files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link application.css
-//= link application.js
+//= link application.js 
 //= link controllers/application.js
 //= link controllers/hello_controller.js
 //= link controllers/index.js
--- db/migrate/20241212090005_create_contacts.rb
+++ db/migrate/20241212090005_create_contacts.rb
@@ -4,6 +4,7 @@ class CreateContacts < ActiveRecord::Migration[8.0]
       t.string :name
       t.string :email
       t.text :comments
+
       t.timestamps
     end
   end
