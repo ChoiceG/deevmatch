21c8552e9bcd23b0c2754cc32c80074e214f3d99
ChoiceG <efechoice@gmail.com>
2024-12-13 18:00:41 +0100

    Updeted production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              "smtp.gmail.com",
   port:                 587,
-  domain:               "https://deevmatch.onrender.com"
+  domain:               "https://deevmatch.onrender.com",
   user_name:            ENV['GMAIL_USERNAME'],
   password:             ENV['GMAIL_PASSWORD'],
   authentication:       'plain',
